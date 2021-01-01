e84044d6ab65015997ef55d8b40292be893bdebb
Ganesh Chandra <cganesh208@gmail.com>
2021-01-01 21:43:07 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,21 @@
+const sounds=['dog','cat','horse','cow','elephant']
+sounds.forEach(sound=>{
+    const btn=document.createElement('button')
+    btn.classList.add('buttn')
+    btn.innerText=sound
+    btn.addEventListener('click',()=>{
+        stopSound()
+        document.getElementById(sound).play()
+    })
+    document.getElementById('buttons').appendChild(btn)
+
+
+})
+function stopSound(){
+    sounds.forEach(sound=>{
+        const song=document.getElementById(sound)
+        song.pause()
+        song.currentTime=0;
+
+    })
+}
